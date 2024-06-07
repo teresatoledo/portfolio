5f6a9556b276fb2a1599e32f8b22d3fcf856f24a
Teresa <teresatoledolara@gmail.com>
2024-06-07 15:44:15 +0200

    Fix footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,9 +8,11 @@ function Footer() {
         Teresa Toledo | 2024
       </div>
       <div className="flex items-center justify-between gap-1 sm:gap-3">
-        <Linkedin className='h-5' href='https://www.linkedin.com/in/teresatoledo' target='_blank'/>
-        <Github className='h-5' href='https://github.com/teresatoledo' target='_blank'/>
-        <Mail className='h-5' href='mailto:teresatoledolara@gmail.com'/>
+        <a href='https://www.linkedin.com/in/teresatoledo' target='_blank'><Linkedin className='h-5' /></a>
+        <a href='https://github.com/teresatoledo' target='_blank'><Github className='h-5' /></a>
+        <a className='h-5' href='mailto:teresatoledolara@gmail.com'><Mail /></a>
+        
+        
       </div>
     </footer>
   )
