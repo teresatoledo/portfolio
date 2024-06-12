b60bdae4597a2d618c3025973f3ceb8255cbbd72
Teresa <teresatoledolara@gmail.com>
2024-06-12 11:54:52 +0200

    Update CV

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { Button } from './ui/button'
 
 function Experience() {
   return ( 
+    <div>
   <div className='max-w-6xl mx-auto sm:px-24 px-6' id="experience">
     <h2 className="font-main font-bold  hover:text-darkblue duration-75 delay-75 text-2xl pt-3 hover:text-3xl md:text-3xl md:pt-8 md:hover:text-4xl pb-8">Experience</h2>
     <ol className="relative border-s border-gray-200 dark:border-gray-700">
@@ -12,6 +14,9 @@ function Experience() {
           <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Over these past two and a half years, I dedicated myself entirely to translating and proofreading scientific and technical texts. This endeavor allowed me to develop crucial skills in the translation industry, attention to detail and curiosity. Additionally, I assumed the role of project manager for certain clients, where I was responsible for tasks such as project preparation, managing delivery schedules, overseeing the team, and resolving any issues that arose.</p>
       </li>
   </ol>
+  
+</div>
+<div className='flex justify-center pb-3'><Button className='bg-darkblue hover:bg-darkblue'><a href="https://drive.google.com/file/d/12q53O_0wjbsNAdMitzVj5gjrb5DmCe92/view" target='_blank'>Download my CV</a></Button></div>
 </div>
   )
 }
